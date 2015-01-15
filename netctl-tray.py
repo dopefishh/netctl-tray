2519ca7920a9c4041a282075a00608b68b9bf076
Mart Lubbers <mart@martlubbers.net>
2015-01-15 18:45:52 +0100

    Raw program done
    Menus implemented
    Skeleton implemented for starting and stopping
    TODO: implement nectl interface

--- netctl-tray.py
+++ netctl-tray.py
@@ -3,7 +3,47 @@ import sys
 import netctl
 
 
+class NetworkAction(QtGui.QAction):
+    """QAction wrapper that can start of stop a profile.
+
+    Variables:
+    tray      - Tray icon object.
+    profile   - Representing profile.
+    profiles  - List of all profiles.
+    """
+    def __init__(self, parent, tray, profile, profiles):
+        super(NetworkAction, self).__init__(parent)
+        self.tray = tray
+        self.profile = profile
+        self.profiles = profiles
+        self.setText(profile['name'])
+        self.setCheckable(True)
+        self.setChecked(profile['active'])
+        self.triggered.connect(self.sig_triggered)
+
+    def sig_triggered(self):
+        """Signal handler for activating the action. In practice this is
+        starting or stopping the profile. When there is another profile active
+        with the same interface it will be stopped."""
+        if self.profile['active']:
+            netctl.stop_profile(self.profile['name'], self.tray)
+        else:
+            concurents = [a for a in self.profiles if
+                          a is not self.profile and
+                          a['interface'] == self.profile['interface'] and
+                          a['active']]
+            for concurent in concurents:
+                netctl.stop_profile(concurent['name'], self.tray)
+            netctl.start_profile(self.profile['name'], self.tray)
+
+
 class NetctlTray(QtGui.QSystemTrayIcon):
+    """QSystemTrayIcon wrapper that control the system tray icon to interface
+    with netctl.
+
+    Variables:
+    connect_menu - QMenu that contains the connections.
+    """
     def __init__(self):
         super(NetctlTray, self).__init__()
         self.setIcon(QtGui.QIcon('icon.svg'))
@@ -22,6 +62,7 @@ class NetctlTray(QtGui.QSystemTrayIcon):
         self.setContextMenu(menu)
 
     def updateMenu(self):
+        """Update the menu according to netctl output and profile files."""
         self.connect_menu.clear()
         self.int_profs = {}
         profiles = sorted(netctl.get_profiles(), key=lambda x: x['interface'])
@@ -30,33 +71,40 @@ class NetctlTray(QtGui.QSystemTrayIcon):
                 self.connect_menu.addSeparator()
                 self.int_profs[profile['interface']] = []
             self.int_profs[profile['interface']].append(profile['name'])
-            item = self.connect_menu.addAction(profile['name'])
-            item.setCheckable(True)
-            if profile['active']:
-                item.setChecked(True)
-            item.triggered.connect
+            self.connect_menu.addAction(
+                NetworkAction(self.connect_menu, self, profile, profiles))
 
     def sig_activated(self, reason):
-        if reason == QtGui.QSystemTrayIcon.Unknown:
-            print('Unknown trigger')
-        elif reason == QtGui.QSystemTrayIcon.Context:
+        """Signal handler for clicking the tray icon.
+
+        Arguments:
+        Reason - QSystemTrayIcon.Reason:
+            RightClick - Spawn the menu.
+            LeftClick  - Show the status.
+            Other      - Do nothing.
+        """
+        if reason == QtGui.QSystemTrayIcon.Context:
             self.updateMenu()
             self.contextMenu().popup(self.geometry().topLeft())
-        elif reason == QtGui.QSystemTrayIcon.DoubleClick:
-            pass
-        else:
+        elif reason == QtGui.QSystemTrayIcon.Click:
             self.showMessage(
                 'Current connection',
                 'connected to: blablabla',
                 QtGui.QSystemTrayIcon.NoIcon, msecs=5000)
 
     def sig_messageClicked(self):
+        """Signal handler for clicking the status popup. When this happens a
+        1ms empty popup will be shown to clear the current popup."""
         self.showMessage('', '', msecs=1)
 
     def sig_exit(self):
+        """Signal handler for the exit button in the main menu. When this
+        happens the program quits."""
         sys.exit()
 
     def sig_about(self):
+        """Signal handler for the about button in the main menu. When this
+        happens the program shows a dialog box containing the about info."""
         QtGui.QMessageBox.information(
             None,
             'About',
--- netctl.py
+++ netctl.py
@@ -1,32 +1,55 @@
+from PySide import QtGui
 import subprocess
 import sys
-import re
 
 netctl_root = '/etc/netctl'
 
 
 def get_profiles():
+    """Give the current profiles registered with netctl.
+
+    Yields:
+        Dictionary for every profile containing at least the keys:
+        active    - Boolean that shows if the profile is currently in use.
+        file      - String containing the profile file location.
+        interface - String containing the interface name.
+        name      - String containing the name of the profile.
+    """
+
     profiles = subprocess.check_output(['netctl', 'list'])
     for profile in profiles.splitlines():
         profile = profile.decode(sys.getdefaultencoding())
-        cp = {}
-        cp['name'] = profile[2:].strip()
-        cp['active'] = profile.startswith('*')
+        cp = {'name': profile[2:].strip(), 'active': profile.startswith('*')}
         cp['file'] = '{}/{}'.format(netctl_root, cp['name'])
         with open(cp['file'], 'r') as f:
             for line in f:
-                interface = re.match('Interface=(?P<int>.*)', f.read())
-                if interface:
-                    cp['interface'] = interface.group('int')
+                if line.startswith('Interface='):
+                    cp['interface'] = line[10:]
                     break
             else:
                 cp['interface'] = 'undetected'
         yield cp
 
 
-def start_profile(profile):
-    print('start: ', profile)
+def start_profile(profile, notify=None):
+    """Starts a profile and optionally notifies the progress.
+
+    Arguments:
+    profile - String containing the name of the profile.
+    """
+    if notify:
+        notify.showMessage(
+            '', 'Starting {}...'.format(profile),
+            QtGui.QSystemTrayIcon.NoIcon, msecs=5000)
 
 
-def end_profile(profile):
-    print('end: ', profile)
+def stop_profile(profile, notify=None):
+    """Stops a profile and optionally notifies the progress.
+
+    Arguments:
+    profile - String containing the name of the profile.
+    """
+    if notify:
+        notify.showMessage(
+            '', 'Stopping {}...'.format(profile),
+            QtGui.QSystemTrayIcon.NoIcon, msecs=5000)
